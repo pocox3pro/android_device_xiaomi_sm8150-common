c32e1ea183a1f51ad4fadd02a7e805bfe914e378
rahul9999xda <rahul9999xda@gmail.com>
2021-07-11 14:17:04 +0000

    sm8150-common: compile kernel with proton clang
    * Also add some kernel build flags
    
    Signed-off-by: rahul9999xda <rahul9999xda@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -88,7 +88,9 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8150
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument" AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
 
 # NFC
 TARGET_USES_NQ_NFC := true
